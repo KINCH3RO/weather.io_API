579fca2cd94363adb6b9e7dd218097bb93c8c9f9
kinch3ro <hat83203@gmail.com>
2022-03-07 17:07:56 +0100

    removing unused code

--- routes/weatherApi.js
+++ routes/weatherApi.js
@@ -1,16 +1,9 @@
 const axios = require('axios').default;
-const fs=require('fs')
+
 let API_KEY = process.env.WEATHER_API_KEY;
-let test = process.env.TEST
 
-function getStartingDate(){
-    const date1 = new Date();
-    date1.setHours(0)
-    date1.setMinutes(0)
-    date1.setSeconds(0)
 
-    return Math.floor(date1.getTime()/1000)
-}
+
 module.exports = (app) => {
 
     app.get('/currentWeatherByName', (req, res) => {
@@ -47,36 +40,65 @@ module.exports = (app) => {
 
 
 
-    app.get('/oneCallWeatherByLoc', (req, res) => {
-        let long = req.query.long;
-        let lat = req.query.lat;
-        if (!long || !lat) {
-            res.status(400).send("Bad request")
 
+
+    app.get('/oneCallWeatherByLoc', (req, res) => {
+
+
+        let units = req.query.units || "metric"
+
+        if (!["standard", "metric", "imperial"].includes(units)) {
+            units = "metric"
+        }
+        let longParam = req.query.lon;
+        let latParam = req.query.lat;
+        if (!longParam || !latParam) {
+            res.status(400).send("Bad request")
             return
         }
-        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${API_key}`).then(response => {
-            res.json(response.data)
-        }).catch(err => {
 
-            res.status(err.response.status).send(err.response.statusText)
+        axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lon=${longParam}&lat=${latParam}&limit=1&appid=${API_KEY}`).then(response => {
+
+
+
+            let long = response.data[0].lon
+            let lat = response.data[0].lat
+            let id = response.data[0].country
+            let locationName = response.data[0].name
+            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${API_KEY}&units=${units}`).then(response => {
+                let data = response.data
+                data["countryId"] = id
+                data["locationName"] = locationName
+                axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${long}&appid=${API_KEY}`).then(response => {
+
+                    data["air_pollution"] = response.data
+                    res.json(data)
+                }).catch(err => {
+                    res.json(data)
+                })
+            }).catch(err => {
+                console.log(err);
+                res.status(404).send("Not found")
+            })
+        }).catch(err => {
+            console.log(err);
+            res.status(404).send("Not found")
         })
     })
 
     app.get('/oneCallWeatherByName', (req, res) => {
-        if(test=="true"){
-            console.log("TEST API MODE");
-          let json=  fs.readFileSync(process.cwd()+"\\response.json",{encoding:'utf8'})
-            res.json(JSON.parse(json))
-            return
+        // if(test=="true"){
+        //     console.log("TEST API MODE");
+        //   let json=  fs.readFileSync(process.cwd()+"\\response.json",{encoding:'utf8'})
+        //     res.json(JSON.parse(json))
+        //     return
 
-        }
+        // }
 
-        
 
         let cityName = req.query.cityName;
         let units = req.query.units || "metric"
-        let dt = req.query.dt || getStartingDate()
+        // let dt = req.query.dt || getStartingDate()
         if (!["standard", "metric", "imperial"].includes(units)) {
             units = "metric"
         }
@@ -84,20 +106,21 @@ module.exports = (app) => {
             res.status(400).send("Bad request")
             return
         }
+
         axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`).then(response => {
             let long = response.data[0].lon
             let lat = response.data[0].lat
-
+            let id = response.data[0].country
+            let locationName = response.data[0].name
             axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${API_KEY}&units=${units}`).then(response => {
                 let data = response.data
-
+                data["countryId"] = id
+                data["locationName"] = locationName
                 axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${long}&appid=${API_KEY}`).then(response => {
 
                     data["air_pollution"] = response.data
                     res.json(data)
                 }).catch(err => {
-
-                    
                     res.json(data)
                 })
             }).catch(err => {
@@ -152,12 +175,17 @@ module.exports = (app) => {
 
     app.get('/getLocationByName', (req, res) => {
         let cityName = req.query.cityName;
+        let full = req.query.full
 
         if (!cityName) {
             res.status(400).send("Bad request")
             return
         }
         axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`).then(response => {
+            if (full) {
+                res.json(response.data)
+                return
+            }
             let long = response.data[0].lon
             let lat = response.data[0].lat
 
